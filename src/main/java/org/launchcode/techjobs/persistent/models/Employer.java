dba3808c0a86f107350f54b6ce28afc79a969e96
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-10-30 23:27:48 -0400

    working on Task 2: Controllers

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message ="Location can not be blank")
+    @Size(max=50)
+    private String location;
+
+//  public accessors
+    public @NotBlank String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+//  No arg constructor
+    public Employer(){}
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String description;
+    //public accessor
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    // no arg constructor
+    public Skill(){}
 }
