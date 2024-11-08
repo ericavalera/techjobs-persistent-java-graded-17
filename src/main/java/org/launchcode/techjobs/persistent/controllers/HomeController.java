788dc4fa45caad99a88bc1139ae56bd8e15a339a
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-11-07 21:54:07 -0500

    Completed most of Task 3

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +22,15 @@ import java.util.Optional;
 @Controller
 public class HomeController {
 
+    @Autowired
+    EmployerRepository employerRepository;
+
+    @Autowired
+    SkillRepository skillRepository;
+
+    @Autowired
+    JobRepository jobRepository;
+
     @RequestMapping("/")
     public String index(Model model) {
 
@@ -28,6 +42,7 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
 	model.addAttribute("title", "Add Job");
+
         model.addAttribute(new Job());
         return "add";
     }
@@ -40,6 +55,17 @@ public class HomeController {
 	    model.addAttribute("title", "Add Job");
             return "add";
         }
+//        ** TODO add code inside this method to select the employer object that has been chosen
+//        to be affiliated with the new job. You will need to select the employer using request parameter
+//        to this method
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
+        //with help of book example
+            if(optEmployer.isPresent()){
+            Employer employer = optEmployer.get();
+            newJob.setEmployer(employer);
+        }
+
+
 
         return "redirect:";
     }
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,14 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
     @NotBlank(message ="Location can not be blank")
     @Size(max=50 )
     private String location;
+
+    //Task 3
+    @OneToMany
+    //this annotation needs a name parameter, usually through "id";
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
 //  public accessors
     public @NotBlank String getLocation() {
